e835ed431eee13aba7d4ab1888493b704d2e662b
Svetlozar Georgiev <svetlozar.georgiev@evnisoft.com>
2024-01-26 15:39:31 +0200

    code fixes

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,17 +1,23 @@
 "use client";
-import React, { useEffect, useState } from "react";
-import { Container, Divider, Stack, Typography, Paper, CardContent, Card, Avatar, useTheme  } from "@mui/material";
+import { useEffect, useState } from "react";
+import {
+  Container,
+  Divider,
+  Stack,
+  Typography,
+  Paper,
+  Avatar,
+  useTheme,
+} from "@mui/material";
 import TextField from "@/components/MUIComponents/TextField";
 import { USERNAME, USER_EMAIL } from "@/helpers/helpers";
 
-
 const ProfilePage = () => {
-  const theme = useTheme()
+  const theme = useTheme();
   const [user, setUser] = useState({
     username: "",
     email: "",
   });
-  const avatarLetter = user.username ? user.username[0].toUpperCase() : ""; 
 
   useEffect(() => {
     if (USERNAME && USER_EMAIL)
@@ -21,18 +27,36 @@ const ProfilePage = () => {
 
   return (
     <Container>
-      <Paper elevation={3} sx={{ padding: 4, marginBottom: 4 }}>
-      <Stack justifyContent="center" alignItems="center" gap={2} mt={2} mb={2}>
-          <Typography variant="h2">Вашият Профил</Typography>
-          <Avatar sx={{ width: 100, height: 100, backgroundColor: theme.palette.grey[900]}}><Typography sx={{fontSize: 70}}>{avatarLetter}</Typography></Avatar>
-            <Typography variant="h4" mb={2}>
-              {user.username ? user.username : "Име Фамилия"}
+      <Paper elevation={3} sx={{ p: 4, mb: 4 }}>
+        <Stack
+          justifyContent="center"
+          alignItems="center"
+          gap={2}
+          mt={2}
+          mb={2}
+        >
+          <Typography component="h2" variant="h2">
+            Вашият Профил
+          </Typography>
+          <Avatar
+            sx={{
+              width: 100,
+              height: 100,
+              backgroundColor: theme.palette.grey[900],
+            }}
+          >
+            <Typography sx={{ fontSize: "2rem" }}>
+              {user.username ? user.username[0].toUpperCase() : ""}
             </Typography>
-          </Stack>
+          </Avatar>
+          <Typography component="h4" variant="h4" mb={2}>
+            {user.username ? user.username : "Име Фамилия"}
+          </Typography>
+        </Stack>
 
-          <Divider sx={{ mb: 4 }} />
+        <Divider sx={{ mb: 4 }} />
 
-        <Typography variant="h4" mb={4}>
+        <Typography component="h4" variant="h4" mb={4}>
           Детайли на профила
         </Typography>
 
